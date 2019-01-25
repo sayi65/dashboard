561e1e8469795bc9cf1dc932e505f3aa904ad496
Sai65 <sayi65@live.cn>
2019-01-25 14:07:42 +0900

    combobox -> ok

--- bs-dashboard/components/Card.vue
+++ bs-dashboard/components/Card.vue
@@ -6,47 +6,108 @@
     class="mx-auto"
     max-width="700">
 
-        <v-card-title class="title white--text">
-            <div class="text-sm-left">
+        <v-card-title 
+            class="title white--text"
+            grid-list-md
+            text-xs-center>
+
+
+            <v-layout 
+                row 
+                justify-space-between
+                align-center>
+                      <v-flex 
+                      xs3
+                      align-center
+                      >
+        状況({{ item.tittle }})
+      </v-flex>
+      <v-flex xs3>
+        <v-chip
+                    v-if="item.status === 0"
+                    @input="onClose(item, menu)"
+                    color="green">
+                    問題なし
+                </v-chip>
+                <v-chip
+                    v-else-if="item.status === 1"
+                    @input="onClose(item, menu)"
+                    color="red">
+                    問題あり
+                </v-chip>
+                <v-combobox
+                    background-color="#26c6da"
+                    v-else
+                    xs4
+                    offset-xs6
+                    open-on-clear
+                    hide-selected
+                    v-model="select"
+                    :items="chipsItem"
+                    class="cmb-static-width"
+                    chips>
+                    <template 
+                        slot="selection" 
+                        slot-scope="data">
+                        <v-chip
+                            :selected="data.selected"
+                            :color="select == '問題なし' ? 'green' : 'red'"
+                            >
+                            {{ data.item }}
+                        </v-chip>
+                    </template>
+                </v-combobox>
+      </v-flex>
+
+            </v-layout>
+
+
+
+            <!-- <v-flex 
+                xs6>
+                    状況({{ item.tittle }})
+            </v-flex>
+            <v-flex 
+                xs4
+                offset-xs2
+                class="text-xs-right">
                 <v-chip
                     :close='close'
                     v-if="item.status === 0"
-                    @input="onClose(item)"
+                    @input="onClose(item, menu)"
                     color="green">
                     問題なし
                 </v-chip>
                 <v-chip
                     :close='close'
                     v-else-if="item.status === 1"
-                    @input="onClose(item)"
+                    @input="onClose(item, menu)"
                     color="red">
                     問題あり
                 </v-chip>
-                <v-menu
+                <v-combobox
+                    background-color="#26c6da"
                     v-else
-                    v-model="showMenu"
-                    origin="center">
-
-                    <v-chip
-                    slot="activator"
-                    ref="chip"
-                    close
-                    readonly
-                    color="red">
-                    問題あり
-                    </v-chip>
-
-                    <v-list>
-                        <v-list-tile
-                        v-for="(chips, i) in chipsItem"
-                        :key="i"
-                        @click="onSelect(chips, item)">
-                        <v-list-tile-title>{{ chips }}</v-list-tile-title>
-                        </v-list-tile>
-                    </v-list>
-                </v-menu>
-            </div>
-            状況({{ item.tittle }})
+                    xs4
+                    offset-xs6
+                    open-on-clear
+                    hide-selected
+                    v-model="select"
+                    :items="chipsItem"
+                    class="cmb-static-width"
+                    chips>
+                    <template 
+                        slot="selection" 
+                        slot-scope="data">
+                        <v-chip
+                            :selected="data.selected"
+                            :color="select == '問題なし' ? 'green' : 'red'"
+                            >
+                            {{ data.item }}
+                        </v-chip>
+                    </template>
+                </v-combobox>
+            </v-flex> -->
         </v-card-title>
 
         <v-card-text class="white">
@@ -72,10 +133,10 @@
                 編集
             </v-btn>
 
-            <v-btn 
-              flat 
+            <v-btn  
               dark 
               small
+              depressed
               color="indigo"
               class="mx-0"
               @click="addBtn(item)"
@@ -102,17 +163,13 @@
         }
     },
     data: () => ({
-     close:false,
      outline:false,
      readonly:true,
      hint: '',
-     chip :true,
-     chipsItem:{
-        0: '問題なし', 
-        1:'問題あり'
-    },
+     chipsItem:[
+        '問題なし', '問題あり'
+     ],
      select:'',
-     showMenu: false
     }),
     methods:{
         toggleBtn(item, event){
@@ -120,16 +177,22 @@
           this.outline =!this.outline
           this.readonly = !this.readonly
           this.hint = this.readonly ? '' : '進捗入力'
+          if(item.status === 3){
+              this.onSelect(this.select, item)
+          } else {
+              item.status = 3
+              this.select = this.select === 0 ? '問題なし' : '問題あり'
+          }
+
         },
         onClose(item){
+            console.log(item)
             if(item.status === 0 || item.status === 1){
                 item.status = 3
                 if(this.select != ''){
                     this.select = ''
                 }
             }
-            console.log(this.$refs)
-            this.showMenu = !this.showMenu
         },
         onSelect(select, item){
 
@@ -142,3 +205,13 @@
     },
   }
 </script>
+
+<style>
+.title{
+    height: 72px !important;
+    padding: 16px 16px 0 16px !important;
+}
+.cmb-static-width {
+    width: 100px !important;
+}
+</style>
